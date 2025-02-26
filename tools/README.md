01f3ce461a6697ec35fec13eb9e2e31480d14e72
Giulio Quaresima <giulio.quaresima@unisi.it>
2025-02-26 17:29:10 +0100

    Issue #4 - script per individuare tag <abbr> errati

---
+++ tools/README.md
@@ -0,0 +1,3 @@
+# Tools
+
+Here you can find tools written in various languages for various purposes, e.g. for debugging, extracting and migrating data, and more.
---
+++ tools/python/issue_4/search_defective_abbr_tag.py
@@ -0,0 +1,19 @@
+import json
+import pathlib
+
+# Ottengo il path della risorsa relativo a questo file python e apro il file
+json_file_path = pathlib.Path(__file__).parent.parent.joinpath('resources/7caba5736b1d9e26c1a5a8af278a3383.json')
+json_file = open(json_file_path, 'r', encoding="utf-8")
+
+# Decoding JSON to a dict
+json_document = json.load(json_file)
+
+# The length of the text
+print("Plain text length: %d" % len(json_document['hits']['hits'][0]['_source']['plainText']))
+
+for tag in (tag for tag in json_document['hits']['hits'][0]['_source']['tags'] if tag['externalName'] == 'abbr'):
+    if (tag['startPosition'] >= tag['endPosition']):
+        print(tag['startPosition'])
+        # print(tag)
+    
+json_file.close()
