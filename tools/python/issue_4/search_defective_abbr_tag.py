f7a0ca53b74bbd85ab170e306422a1a7db97a720
Giulio Quaresima <giulio.quaresima@unisi.it>
2025-03-03 18:38:08 +0100

    new resource

--- tools/python/issue_4/search_defective_abbr_tag.py
+++ tools/python/issue_4/search_defective_abbr_tag.py
@@ -2,17 +2,19 @@ import json
 import pathlib
 
 # Ottengo il path della risorsa relativo a questo file python e apro il file
-json_file_path = pathlib.Path(__file__).parent.parent.joinpath('resources/7caba5736b1d9e26c1a5a8af278a3383.json')
+# json_file_path = pathlib.Path(__file__).parent.parent.joinpath('resources/7caba5736b1d9e26c1a5a8af278a3383.json')
+json_file_path = pathlib.Path(__file__).parent.parent.joinpath('resources/b76f9b1a74d8c152bfc33126bb614db6.json')
 json_file = open(json_file_path, 'r', encoding="utf-8")
 
 # Decoding JSON to a dict
 json_document = json.load(json_file)
 
 # The length of the text
-plainText = json_document['hits']['hits'][0]['_source']['plainText']
+# plainText = json_document['hits']['hits'][0]['_source']['plainText']
+plainText = json_document['_source']['plainText']
 print("Plain text length: %d" % len(plainText))
 
-for tag in (tag for tag in json_document['hits']['hits'][0]['_source']['tags'] if tag['externalName'] == 'abbr'):
+for tag in (tag for tag in json_document['_source']['tags'] if tag['externalName'] == 'abbr'):
     if (tag['startPosition'] >= tag['endPosition']):
         # print(tag['startPosition'])
         print(tag)
--- tools/python/resources/README.md
+++ tools/python/resources/README.md
@@ -1,7 +1,7 @@
 # Resources
 
 Comando di esempio per estrarre i documenti JSON da ElasticSearch
+```bash
+curl localhost:9200/dasmemo2/document/7caba5736b1d9e26c1a5a8af278a3383 > 7caba5736b1d9e26c1a5a8af278a3383.json
+# curl -H 'Content-Type: application/json' localhost:9200/dasmemo2/_search?pretty -d '{"query":{"term":{"documentId":"7caba5736b1d9e26c1a5a8af278a3383"}}}' > 7caba5736b1d9e26c1a5a8af278a3383.json
 ```
-curl -H 'Content-Type: application/json' localhost:9200/dasmemo2/_search?pretty -d '{"query":{"term":{"documentId":"7caba5736b1d9e26c1a5a8af278a3383"}}}' > 7caba5736b1d9e26c1a5a8af278a3383.json
-```
-
